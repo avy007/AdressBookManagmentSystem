88a731b70ae34b2799685c8d5f7c6e2f2af1379f
akash <akashyewale007@gmail.com>
2023-03-05 00:06:45 +0530

    editcontactInAdressbook

--- src/com/adressbook/AdressBookSystem.java
+++ src/com/adressbook/AdressBookSystem.java
@@ -1,60 +1,137 @@
 package com.adressbook;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AdressBookSystem {
+	public static ArrayList<Contact> cotacts_array=new ArrayList<>();
 
-	public static void main(String[] args) {
-		
-		System.out.println("welcome to adress book");
-		
-		add();
+    public static void main(String[] args) {
+        System.out.println("---   Welcome to Address Book   ---");
+      
+        //Scanner sc =new Scanner(System.in);
+        //int input =sc.nextInt();
 
-	}
-	 static void add(){
-	        Scanner sc=new Scanner(System.in);
+        add();
+        edit();
+    
 
-	        System.out.println("Enter First Name :");
-	        String name=sc.nextLine();
 
-	        System.out.println("Enter Last Name :");
-	        String last_n=sc.nextLine();
+    }
 
-	        System.out.println("Enter City :");
-	        String city = sc.nextLine();
 
-	        System.out.println("Enter Number :");
-	        String number = sc.nextLine();
+    static void add(){
+//        AddressBook addressBook=new AddressBook();
+        Scanner sc=new Scanner(System.in);
 
-	        System.out.println("Enter email :");
-	        String email=sc.nextLine();
+        System.out.println("Enter First Name :");
+        String name=sc.nextLine();
 
-	        contact Contact = new contact(name, last_n, city, number, email);
-	        System.out.println("Contact Added Successfully...");
+        System.out.println("Enter Last Name :");
+        String last_n=sc.nextLine();
 
-	    }
+        System.out.println("Enter City :");
+        String city = sc.nextLine();
 
+        System.out.println("Enter Number :");
+        String number = sc.nextLine();
+
+        System.out.println("Enter email :");
+        String email=sc.nextLine();
+
+        Contact contact = new Contact(name, last_n, city, number, email);
+        cotacts_array.add(contact);
+        System.out.println("Contact Added Successfully...");
+
+    }
+    static void edit() {
+        System.out.println("Enter Name to check");
+        Scanner sc = new Scanner(System.in);
+        String newName = sc.nextLine();
+//Contact contact;
+        for(Contact contact : cotacts_array){
+            if(newName.equals(contact.getName())){
+
+                System.out.println("Enter Update First Name :");
+                String New_name=sc.nextLine();
+
+                System.out.println("Enter Update Last Name :");
+                String New_last_n=sc.nextLine();
+
+                System.out.println("Enter Update City :");
+                String New_city = sc.nextLine();
+
+                System.out.println("Enter Update Number :");
+                String New_number = sc.nextLine();
+
+                System.out.println("Enter Update email :");
+                String New_email=sc.nextLine();
+
+                contact.setName(New_name);
+                contact.setLast_N(New_last_n);
+                contact.setCity(New_city);
+                contact.setNumber(New_number);
+                contact.setEmail(New_email);
+
+                System.out.println("Update ...");
+            }else {
+                System.out.println("Sorry Not found");
+            }
+        }
+//        Contact contact =Contact.getName();
+    }
+}
+
+
+class Contact{
+    //String name,String lasttN,String city,String number,String email
+    private String name;
+    private String last_N;
+    private String city;
+    private String number;
+    private String email;
+
+    public Contact(String name,String lasttN,String city,String number,String email){
+        this.name = name;
+        this.last_N = lasttN;
+        this.city = city;
+        this.number = number;
+        this.email = email;
+    }
+
+   public String getName(){return name;}
+    public String getLast_N(){return last_N;}
+    public String getCity(){return city;}
+    public String getNumber(){return number;}
+    public String getEmail(){return email;}
+
+    public String setName(String new_name){
+        this.name=new_name;
+        return new_name;
+    }
+
+    public String setLast_N(String new_Last_N){
+        this.last_N=new_Last_N;
+        return new_Last_N;
+    }
+
+    public String setCity(String new_City){
+        this.city=new_City;
+        return new_City;
+    }
+
+    public String setNumber(String new_Number){
+        this.name=new_Number;
+        return new_Number;
+    }
+    public String setEmail(String new_Email){
+        this.email=new_Email;
+        return new_Email;
+    }
 
 }
-class contact{
-	
-	private String name;
-	private String last_N;
-	private String city;
-	private String number;
-	private String email;
-	
-	public contact(String name,String last_N,String city,String number,String email) {
-		
-		this.name=name;
-		this.last_N=last_N;
-		this.city=city;
-		this.number=number;
-		this.email=email;
-		
-	}
 	
 	
 	
 	
-}
+
