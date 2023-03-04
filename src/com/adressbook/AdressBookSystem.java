73908226fbb21dbe24e9484ccfae16d867e0f837
akash <akashyewale007@gmail.com>
2023-03-04 23:02:12 +0530

    creatContactInAdressbook

--- src/com/adressbook/AdressBookSystem.java
+++ src/com/adressbook/AdressBookSystem.java
@@ -9,3 +9,25 @@ public class AdressBookSystem {
 	}
 
 }
+class contact{
+	
+	private String name;
+	private String last_N;
+	private String city;
+	private String number;
+	private String email;
+	
+	public contact(String name,String last_N,String city,String number,String email) {
+		
+		this.name=name;
+		this.last_N=last_N;
+		this.city=city;
+		this.number=number;
+		this.email=email;
+		
+	}
+	
+	
+	
+	
+}
